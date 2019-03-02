f6964685c05b2ec90363a091da72d28f47db2a4f
StephenSoft <Stephenzhao94@163.com>
2019-03-03 02:20:35 +0800

    修改之前页面布局及内容,添加contact方法并测试且完成测试,修改根路由

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,6 +7,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = " | Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get static_pages_home_url #FILL_IN
+    assert_response :success  #FILL_IN 使用FILL_IN测试无法通过 根路由即为home方法路由 
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -23,6 +28,12 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About#{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact#{@base_title}"
   end
 
 end
