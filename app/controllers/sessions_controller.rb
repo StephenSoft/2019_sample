c7cf5ca8e53ba1b2d366775bc1dc502bea7f72fb
StephenSoft <Stephenzhao94@163.com>
2019-03-25 22:35:18 +0800

    添加验证用户是否登陆功能，添加详细下拉菜单

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      login(user)
+      Rails.logger.info(session[:user_id])
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 不完全正确”
+      render 'new'
+      #redirect_to signup_path 重定向发起新的post请求
+    end
+  end
+
+  def destroy
+
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+  #登陆用户
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  # 获取当前用户current_user
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id:session[:user_id])
+    end
+  end
+
+  # 判断是否登陆
+  def logged_in?
+    !current_user.nil?
+  end
 end
